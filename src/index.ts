6d1577cf8377aeca48970352efdae8e7504d7c7f
emir4169 <89991965+emir4169@users.noreply.github.com>
2024-06-21 22:08:30 +0300

    actual initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,11 @@
-import { Injector, Logger, webpack } from "replugged";
-
+import { Injector, common } from "replugged";
 const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
 
-export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
-  }
+export function start(): void {
+  inject.before(common.messages, "sendMessage", (args) => {
+    args[1].content = `@silent ${args[1].content}`;
+    return args;
+  });
 }
 
 export function stop(): void {
